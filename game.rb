d65f636979c4cb7557a1ae24deb30e25da64aea3
Katsudon27 <katsudon1127@gmail.com>
2026-05-04 14:05:16 +1000

    Add implementation for player class

--- game.rb
+++ game.rb
@@ -1,11 +1,19 @@
+class Player
+  attr_reader :symbol
+
+  def initialize(symbol)
+    @symbol = symbol
+  end
+end
+
 class Cell
   attr_reader :value
-  
+
   def initialize(value)
     @value = value
   end
 
   def addPiece(player)
-    
+    @value = player.symbol
   end
 end
