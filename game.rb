a5379733e525d1508dc99a630c266352b61c8f6b
Katsudon27 <katsudon1127@gmail.com>
2026-05-05 11:00:04 +1000

    Add methods to enforce game rules
    Added methods for checking win conditions and move validity

--- game.rb
+++ game.rb
@@ -10,15 +10,17 @@ end
 class Cell
   @@number_of_cells = 0
 
-  attr_reader :value
+  attr_reader :value, :is_occupied
 
   def initialize()
     @@number_of_cells += 1
     @value = @@number_of_cells.to_s
+    @is_occupied = false
   end
 
   def addPiece(player)
     @value = player.symbol
+    @is_occupied = true
   end
 end
 
@@ -49,6 +51,22 @@ class GameBoard
       puts row.map { |cell| cell.value }.join(" ")
     end
   end
+
+  def returnEmptyCells
+    @board.map do |row|
+      row.filter do |cell|
+        cell.is_occupied === false ? cell.value : next
+      end.map {|cell| cell.value}
+    end.flatten
+  end
+
+  def returnPlayerCells(player)
+    @board.map do |row|
+      row.filter do |cell|
+        cell.value === player.symbol
+      end.map {|cell| cell.value}
+    end.flatten
+  end
 end
 
 class GameController
@@ -67,11 +85,35 @@ class GameController
     @game_board.printBoard()
     puts "#{@currentPlayer.name}'s turn: Please choose a valid cell (0 - 9) to place your piece"
   end
+
+  def checkMoveValidity(cell_num)
+    @game_board.returnEmptyCells().include?(cell_num) ? true : false
+  end
+
+  def checkWin(player)
+    player_moves = @board.returnPlayerCells()
+    winning_conditions_row = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+    winning_conditions_column = [["1", "4", "7"], ["2", "5", "8"], ["3", "6", "9"]]
+    winning_conditions_diagonal = [["1", "5", "9"], ["3", "5", "7"]]
+
+    winning_conditions_row.each do |condition|
+      if (condition - player_moves).empty?
+        return true
+      end
+    end
+
+    winning_conditions_column.each do |condition|
+      if (condition - player_moves).empty?
+        return true
+      end
+    end
+
+    winning_conditions_diagonal.each do |condition|
+      if (condition - player_moves).empty?
+        return true
+      end
+    end
+
+    return false
+  end
 end
-
-#test = GameBoard.new()
-#test.printBoard
-
-#player1 = Player.new("X")
-#test.placePiece("1", player1)
-#test.printBoard
