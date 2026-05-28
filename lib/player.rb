0dcacc2cbcf126816bc3c71ad34d24d295793078
Katsudon27 <katsudon1127@gmail.com>
2026-05-28 11:34:44 +1000

    Refactor and move input logic to player class

--- lib/player.rb
+++ lib/player.rb
@@ -5,4 +5,10 @@ class Player
     @name = name
     @symbol = symbol
   end
+
+  def make_move
+    puts "#{@name}'s turn: Please choose a valid cell (0 - 9) to place your piece"
+
+    gets.chomp
+  end
 end
