640460bbd48e2062abdb779cc601cf0ae1f2ea46
Katsudon27 <katsudon1127@gmail.com>
2026-05-30 12:15:48 +1000

    Add unit test for #switch_turn

--- spec/game_controller_spec.rb
+++ spec/game_controller_spec.rb
@@ -3,12 +3,12 @@ require_relative "../lib/game_board"
 require_relative "../lib/player"
 
 describe GameController do
-  describe "#player_win?" do
-    let(:player1) { instance_double(Player) }
-    let(:player2) { instance_double(Player) }
-    let(:game_board) { instance_double(GameBoard) }
-    subject(:game_controller) { described_class.new(player1, player2, game_board) }
+  let(:player1) { instance_double(Player) }
+  let(:player2) { instance_double(Player) }
+  let(:game_board) { instance_double(GameBoard) }
+  subject(:game_controller) { described_class.new(player1, player2, game_board) }
 
+  describe "#player_win?" do
     context "when a player places three pieces across a row" do
       before do
         allow(game_controller).to receive(:current_player_cells).and_return(%w[1 2 3])
@@ -43,6 +43,13 @@ describe GameController do
       it "should return false" do
         expect(game_controller.player_win?).to be false
       end
+    end
+  end
+
+  describe "#switch_turn" do
+    it "should switch to the other player as the current player" do
+      game_controller.switch_turn
+      expect(game_controller.current_player).to eq(player2)
     end
   end
 end
